9cbdf750bafa882bf42ccba2ae1f5a46fab048bb
angela-araujo <angela.araujo.rj@gmail.com>
2022-01-05 17:21:39 -0300

    Alterando âncoras <a href>  por <Link to> do router-dom para não recarregar página

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import imagem from "../assets/img/doguito.svg";
 import '../assets/css/componentes/cabecalho.css';
 
@@ -11,18 +12,18 @@ const Cabecalho = () => {
             </div>
 
             <div className="cabecalho-container">
-                <a href="/" className="flex flex--centro">
+                <Link to="/" className="flex flex--centro">
                     <img className="cabecalho__logo" src={imagem} alt="Logo" />
                     <h1 className="cabecalho__titulo">PetShop</h1>
-                </a>
+                </Link>
             </div>
         
             <nav className="menu-cabecalho">
                 <ul className="menu-itens">
-                    <li><a href="#" className="menu-item menu-item--entrar">Entrar</a></li>
-                    <li><a href="/produtos" className="menu-item">Produtos</a></li>
-                    <li><a href="/blog" className="menu-item">Blog</a></li>
-                    <li><a href="/sobre" className="menu-item">Sobre</a></li>
+                    <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+                    <li><Link to="/produtos" className="menu-item">Produtos</Link></li>
+                    <li><Link to="/blog" className="menu-item">Blog</Link></li>
+                    <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
                 </ul>
             </nav>
 
