f6d95689257f43a3662706b90f2c32959262515f
angela-araujo <angela.araujo.rj@gmail.com>
2022-01-05 17:12:14 -0300

    Adicionando NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './assets/css/base/base.css'
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './components/Cabecalho';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
 
   return (
     <Router>
-
+      <Cabecalho />
       <Switch>
         <Route exact path="/"><Home /></Route>
         <Route path="/sobre"><Sobre /></Route>
